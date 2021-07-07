85035efec13688c3b9daebc326778df7d34aeea3
jlbuild <juliabuildbot@gmail.com>
2021-07-07 07:40:01 +0000

    ROCmDeviceLibs_jll build 4.0.0+0

---
+++ src/ROCmDeviceLibs_jll.jl
@@ -0,0 +1,9 @@
+# Use baremodule to shave off a few KB from the serialized `.ji` file
+baremodule ROCmDeviceLibs_jll
+using Base
+using Base: UUID
+import JLLWrappers
+
+JLLWrappers.@generate_main_file_header("ROCmDeviceLibs")
+JLLWrappers.@generate_main_file("ROCmDeviceLibs", UUID("873c0968-716b-5aa7-bb8d-d1e2e2aeff2d"))
+end  # module ROCmDeviceLibs_jll
